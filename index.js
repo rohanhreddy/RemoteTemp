6549970731eb78c94a055a57acd449e22b3d0b0d
Rohan <117934079+rohanhreddy@users.noreply.github.com>
2023-04-16 11:54:52 +0530

    gpu works, cpu possibly scrapped?

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const sysInfo = require('systeminformation');
 
-//CPU temp log
-
-sysInfo.cpuTemperature()
-
-.then(data => {
-    console.log("CPU: " + Math.round(data.main) + "°C");
-})
-
-.catch(error => console.error(error));
-
 //GPU temp log while keeping in mind the number of gpus
 
 sysInfo.graphics()
+    .then(data => {
+        data.controllers.forEach(controller => {
+            if (controller.temperatureGpu) {
+                console.log("GPU:", controller.model +",", "Temperature:", controller.temperatureGpu)
+            }
+        })
+    })
+    
+    
+    .catch(error => console.error(error));
 
-.then(data => {
-    data.controllers.forEach((controller, no) => {
-        console.log("GPU " + (no + 1) + " temperature: " + Math.round(controller.temperature) + "°C");
-    });
-})
 
-.catch(error => console.error(error));
